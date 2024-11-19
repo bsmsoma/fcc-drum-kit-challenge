3e012cae48a9eac6e04994d371b081658365b4a0
bsmsoma <soma.bsm@gmail.com>
2024-11-19 19:23:12 -0300

    starting the fcc drum kit

---
+++ fcc-drumkit/src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+export default function App() {
+
+    const data = [
+        {
+            text: "Q",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+            name: "Heater"
+        },
+        {
+            text: "W",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+            name: "Heater 2"
+        },
+        {
+            text: "E",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+            name: "Heater 3"
+        },
+        {
+            text: "A",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+            name: "Heater 4"
+        },
+        {
+            text: "S",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+            name: "Clap"
+        },
+        {
+            text: "D",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+            name: "Open-HH"
+        },
+        {
+            text: "Z",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+            name: "Kick-n'-Hat"
+        },
+        {
+            text: "X",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+            name: "Kick"
+        },
+        {
+            text: "C",
+            audioUrl: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+            name: "Closed-HH"
+        },
+
+    ];
+
+    const drumItems = data.map(item => {
+        return (
+            <div
+                className="drum-pad"
+                id={item.text}>
+                {item.text} {/*The Div Text*/}
+                <audio
+                    className="clip"
+                    src={item.audioUrl}
+                    id={item.text}>
+                </audio>
+            </div>
+        )
+    })
+
+    return (
+        <body id="drum-machine">
+            <div className="padbank"></div>
+            <div className="controlscontainer"></div>
+        </body>
+    )
+}
